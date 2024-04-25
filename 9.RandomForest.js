f4e476901b51366f37f30176cf3141cf9988f428
jvaldiviesob <jvaldiviesob@users.noreply.github.com>
2024-04-25 09:46:42 -0400

    Create 9.RandomForest.js

---
+++ 9.RandomForest.js
@@ -0,0 +1,33 @@
+var firstYear=require('users/corfobbppciren2023/Humedad_de_Suelo:0.firstYear');
+var predictores=require('users/corfobbppciren2023/Humedad_de_Suelo:8.predictores');
+
+// Make a Random Forest classifier and train it
+var clasificador = ee.Classifier.smileRandomForest({
+  numberOfTrees:20,
+  minLeafPopulation:1, 
+  bagFraction:0.5,
+  seed:0
+}).setOutputMode('REGRESSION')
+    .train({
+      features: predictores.training,
+      classProperty: 'soil moisture',
+      inputProperties: [
+                      "apei",
+                      "Preci",
+                      'Tair',
+                      'Evapo',
+                      'LST_DAILY','LST_Diff',
+                      'EVI_SG_linear','NDVI_SG_linear',
+                      'clay','sand','silt',
+                      'TI','elevation',
+                      "lat","lon",
+                      'porosity',
+                      "omc"
+                      ,'WTD'
+                      ,'DTB'
+                      ]
+    });
+//calculate the importance of every land surface feature
+var importance=clasificador.explain();
+
+exports.clasificador = clasificador;
